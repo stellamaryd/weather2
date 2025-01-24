d3c0515b42d45f80e91d11f6011739a957f35ed4
stellamaryd <stelladev06@gmail.com>
2025-01-24 09:10:58 +0000

    Add files via upload

---
+++ Stella/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+	headers: {
+		'x-rapidapi-key': 'ca2b874515msh92c1761fb4c3038p1f6135jsnb1e15c1cbba9',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8362367d13f8229b3035889d21fdde57"; // enter your key from openweather API~~
